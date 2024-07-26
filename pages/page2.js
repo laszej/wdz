59ae27d882a7d3f409de1ac126c7f8297fbe19fb
laszej <stanislawlaskowski87@gmail.com>
2024-07-26 04:33:01 +0200

    o

--- pages/page2.js
+++ pages/page2.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Page2 = () => {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    // Funkcja do aktualizacji szerokości okna
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Aktualizacja szerokości okna po zamontowaniu komponentu
+    updateWindowWidth();
+
+    // Aktualizacja szerokości okna przy każdej zmianie rozmiaru okna
+    window.addEventListener('resize', updateWindowWidth);
+
+    // Czyszczenie po odmontowaniu komponentu
+    return () => {
+      window.removeEventListener('resize', updateWindowWidth);
+    };
+  }, []);
+
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
       <div style={{ textAlign: "center", width: "90%" }}>
@@ -14,7 +34,7 @@ const Page2 = () => {
             maxWidth: "600px", // maksymalna szerokość dla zdjęcia
             display: "block", // umożliwia użycie margin: 0 auto do wyśrodkowania obrazu na większych ekranach
             margin: "0 auto", // centrowanie obrazu na większych ekranach
-            ...(window.innerWidth <= 350 && { maxWidth: "350px" }) // warunkowe ustawienie szerokości dla smartfonów
+            ...(windowWidth <= 350 && { maxWidth: "350px" }) // warunkowe ustawienie szerokości dla smartfonów
           }}
           alt="WDZ Logo"
         />
