2e8cccfb7034617808f69c530b85c590164f57c2
laszej <stanislawlaskowski87@gmail.com>
2025-06-04 21:13:04 +0200

    g analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,39 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import Head from "next/head";
 import Layout from "../components/Layout";
+import Script from "next/script"; // ← dodaj to
 
 function MyApp({ Component, pageProps }) {
-  useEffect(()=>{
+  useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
-  },[])
+  }, []);
 
   return (
     <>
       <Head>
-       <title>Grupa radnych WdZ Osiedle Nowe Winogrady Poznań</title>
+        <title>Grupa radnych WdZ Osiedle Nowe Winogrady Poznań</title>
       </Head>
-      
-      
+
+      {/* Google Analytics (GA4) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-ZGRTE61ETK`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZGRTE61ETK', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
