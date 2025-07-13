5c13815e40d4b8e1d449e7b5c039ad6f11df0c46
laszej <stanislawlaskowski87@gmail.com>
2025-07-13 04:50:39 +0200

    k

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "scontent-waw2-1.xx.fbcdn.net",
-      "scontent-waw2-2.xx.fbcdn.net",
-      "scontent.fwaw1-1.fna.fbcdn.net",
-      "scontent.xx.fbcdn.net",
-      "scontent.cdninstagram.com",
-    ],
-  },
+  
 };
 
 module.exports = nextConfig;
