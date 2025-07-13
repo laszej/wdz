6dce2843c20d8a9ee1fbb0f068eebe0c6790c0d0
laszej <stanislawlaskowski87@gmail.com>
2025-07-13 03:15:40 +0200

    jebać chinoli

--- pages/admin/takealook.js
+++ pages/admin/takealook.js
@@ -2,29 +2,15 @@ import Link from 'next/link';
 
 export default function RecommendationsPage() {
   return (
-    <div className="container">
+    <div>
       <h1>Przejrzyj również to</h1>
-      
-      <div className="links-container">
-        {/* Dodaj tutaj linki do istniejących podstron */}
-        <Link href="/chartExample" className="link-box">
-          <div>stary wykres wyników wyborów</div>
-        </Link>
-        
-        <Link href="/services" className="link-box">
-          <div>Usługi</div>
-        </Link>
-        
-        <Link href="/contact" className="link-box">
-          <div>Kontakt</div>
-        </Link>
-        
-        <Link href="/products" className="link-box">
-          <div>Produkty</div>
-        </Link>
-        
-      </div>
 
+      <ul>
+        <li><Link href="/chartExample">Stary wykres wyników wyborów</Link></li>
+        <li><Link href="/another">pozniej dodam</Link></li>
+        <li><Link href="/another">pozniej dodam</Link></li>
+        <li><Link href="/another">pozniej dodam</Link></li>
+      </ul>
     </div>
   );
 }
