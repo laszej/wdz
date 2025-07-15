7cdefb3ae3e502689e9e89c104535b4e4c69b3c6
laszej <stanislawlaskowski87@gmail.com>
2025-07-15 14:38:31 +0200

    l

--- pages/admin/takealook.js
+++ pages/admin/takealook.js
@@ -5,7 +5,7 @@ export default function RecommendationsPage() {
     <div>
       <h1>Przejrzyj również to</h1>
 
-      <p>Ta strona została zbudowana od zera (jest ewolucją wielu "bliźniaczych" projektów).
+      <p>Ta strona została zbudowana od zera (jest ewolucją wielu bliźniaczych projektów).
          W teorii więc można na niej umieścić wszystko, dlatego będę wrzucał tu tylko propozycje</p>
 
       <ul>
