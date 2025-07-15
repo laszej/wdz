4bc83341fd01c78ea901ec7399547db5491a8f64
laszej <stanislawlaskowski87@gmail.com>
2025-07-15 14:32:24 +0200

    l

--- pages/admin/takealook.js
+++ pages/admin/takealook.js
@@ -15,6 +15,7 @@ export default function RecommendationsPage() {
         <li><Link href="/another">pozniej dodam</Link></li>
         <li><Link href="/another">pozniej dodam</Link></li>
         <li><Link href="/another">pozniej dodam</Link></li>
+        <li><Link href="/another">pozniej dodam</Link></li>
       </ul>
     </div>
   );
