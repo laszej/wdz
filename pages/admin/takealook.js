0a744775cdda1644c2fddea3b6cd73a7c922b4fd
laszej <stanislawlaskowski87@gmail.com>
2025-07-15 14:25:13 +0200

    g

--- pages/admin/takealook.js
+++ pages/admin/takealook.js
@@ -5,10 +5,14 @@ export default function RecommendationsPage() {
     <div>
       <h1>Przejrzyj również to</h1>
 
+      <p>Ta strona została zbudowana od zera (jest ewolucją wielu "bliźniaczych" projektów).
+         W teorii więc można na niej umieścić wszystko, dlatego będę wrzucał tu tylko propozycje</p>
+
       <ul>
         <li><Link href="/chartExample">Stary wykres wyników wyborów</Link></li>
         <li><Link href="/anotherchart">anotherchart</Link></li>
                         
+        <li><Link href="/another">pozniej dodam</Link></li>
         <li><Link href="/another">pozniej dodam</Link></li>
         <li><Link href="/another">pozniej dodam</Link></li>
       </ul>
